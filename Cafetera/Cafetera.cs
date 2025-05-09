c5b981127b43511b522047eed7d5e2c936befd2f
Alex_ <Alex_@ALEX>
2025-05-08 22:27:36 -0300

    Agregar archivos de proyecto.

---
+++ Cafetera/Cafetera.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+
+namespace Cafetera
+{
+	/// <summary>
+	/// Description of Cafetera.
+	/// </summary>
+	public class Cafetera
+	{
+		private int _capacidadMaxima = 0;
+		private int _cantidadActual = 0; 
+		
+		public Cafetera()
+		{
+			_capacidadMaxima = 1000;
+			_cantidadActual = 0;
+		}
+		
+		//                  1500
+		public Cafetera(int capacidad) {
+			_cantidadActual = capacidad;
+			_capacidadMaxima = capacidad;
+		}
+		
+		//                   2000           3000 
+		public Cafetera(int capacidad, int cantidad) {
+			_cantidadActual = cantidad;
+			_capacidadMaxima = capacidad;
+			
+			if (_cantidadActual > _capacidadMaxima) {
+				_cantidadActual = _capacidadMaxima;
+			}
+		}
+		
+		public int getCapacidad() {
+			return _capacidadMaxima;
+		}
+
+		public int getCantidad() {
+			return _cantidadActual;
+		}
+		
+		public void setCantidad(int cantidad) {
+			_cantidadActual = cantidad;
+		}
+		
+		public void setCapacidad(int capacidad) {
+			_capacidadMaxima = capacidad;
+		}
+		
+		public void llenarCafetera() {
+			_cantidadActual = _capacidadMaxima;
+		}
+		
+		public int servirTaza(int taza) {
+			if (taza <= _cantidadActual) {
+				// 400 = 500 - 100 
+				// _cantidadActual -= taza;
+				_cantidadActual = _cantidadActual - taza;
+				return taza;
+			}
+			
+			int aux = _cantidadActual;
+			_cantidadActual = 0;
+			return aux;
+		}
+		
+		public void vaciarCafetera() {
+			_cantidadActual = 0;
+		}
+		
+		public void agregarCafe(int cafe) {
+			_cantidadActual = _cantidadActual + cafe;
+		}
+	}
+}
---
+++ Cafetera/MainForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Cafetera
+{
+	public partial class MainForm : Form
+	{
+		Cafetera cafetera_1;
+		Cafetera cafetera_2;
+		Cafetera cafetera_3;
+		
+		public MainForm()
+		{
+			InitializeComponent();
+			
+			cafetera_1 = new Cafetera();
+			cafetera_2 = new Cafetera(1500);
+			cafetera_3 = new Cafetera(2000, 3000);
+			
+			progressBar1.Value = cafetera_3.getCantidad();
+			label1.Text = cafetera_3.getCantidad().ToString();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			cafetera_3.llenarCafetera();
+			progressBar1.Value = cafetera_3.getCantidad();
+			label1.Text = cafetera_3.getCantidad().ToString();
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			string aux = textBox1.Text;
+			int cantidad = int.Parse(aux);
+			int taza = cafetera_3.servirTaza(cantidad);
+			
+			progressBar1.Value = cafetera_3.getCantidad();
+			label1.Text = cafetera_3.getCantidad().ToString();
+			
+			if (cantidad != taza) {
+				MessageBox.Show("no alcanza");
+			}
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			cafetera_3.vaciarCafetera();
+			
+			progressBar1.Value = cafetera_3.getCantidad();
+			label1.Text = cafetera_3.getCantidad().ToString();
+		}
+		
+		void Button4Click(object sender, EventArgs e)
+		{
+			string aux = textBox2.Text;
+			int cantidad = int.Parse(aux);
+			
+			int cantidadCafetera = cafetera_3.getCantidad();
+			int capacidadCafetera = cafetera_3.getCapacidad();
+			int total = cantidadCafetera + cantidad;
+			
+			if (total <= capacidadCafetera) {
+				cafetera_3.agregarCafe(cantidad);
+				progressBar1.Value = cafetera_3.getCantidad();
+				label1.Text = cafetera_3.getCantidad().ToString();
+			}
+		}
+	}
+}
